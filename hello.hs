8e9994f92afea1a5019100b7bcc569a0edc3ae20
Javier Pérez Arteaga <surtich@gmail.com>
2023-10-27 01:07:59 +0200

    3.1. Flexible HTML content (functions)

--- hello.hs
+++ hello.hs
@@ -1,3 +1,15 @@
+-- hello.hs
+
 main = putStrLn myhtml
 
-myhtml = "<html><body>Hello, world!</body></html>"
+myhtml = makeHtml "Hello title" "Hello, world!"
+
+makeHtml title content = html_ (head_ (title_ title) <> body_ content)
+
+html_ content = "<html>" <> content <> "</html>"
+   
+body_ content = "<body>" <> content <> "</body>"
+
+head_ content = "<head>" <> content <> "</head>"
+
+title_ content = "<title>" <> content <> "</title>"
